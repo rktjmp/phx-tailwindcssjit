8e442beea5edbcc59609074d2da2673709ea3bd6
Oliver Marriott <hello@omarriott.com>
2021-03-30 14:52:15 +1100

    Setup tailwindcss-jit

---
+++ assets/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+    '../lib/**/*.ex',
+    '../lib/**/*.leex',
+    '../lib/**/*.lexs',
+    '../lib/**/*.exs',
+    '../lib/**/*.eex',
+    './js/**/*.js'
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
